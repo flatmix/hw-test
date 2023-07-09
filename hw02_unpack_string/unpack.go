2b73e0fe05fdfdd8f6f7af7a4442633d97c5e1e2
Владислав Тащеев <v.tashcheev@automacon.ru>
2023-07-09 18:55:36 +0300

    HW2 is completed

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -1,12 +1,62 @@
-package hw02unpackstring
+package main
 
 import (
 	"errors"
+	"strconv"
+	"strings"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(inStr string) (string, error) {
+
+	outStr := strings.Builder{}
+	formatStr := []rune(inStr)
+	writeSym := ""
+	ecran := false
+
+	for _, sym := range formatStr {
+
+		num, errNum := strconv.Atoi(string(sym))
+
+		if string(sym) == `\` && !ecran {
+			if len(writeSym) > 0 {
+				outStr.WriteString(writeSym)
+				writeSym = ""
+			}
+			ecran = true
+			continue
+		}
+
+		if errNum == nil && !ecran {
+			if len(writeSym) > 0 {
+				if num > 0 {
+					outStr.WriteString(strings.Repeat(writeSym, num))
+				}
+				writeSym = ""
+			} else {
+				return "", ErrInvalidString
+			}
+		} else {
+			if ecran && string(sym) != `\` && errNum != nil {
+				return "", ErrInvalidString
+			}
+			if len(writeSym) > 0 {
+				outStr.WriteString(writeSym)
+				writeSym = ""
+			}
+
+			if string(sym) != `\` || ecran {
+				writeSym = string(sym)
+				ecran = false
+			}
+
+		}
+	}
+	if len(writeSym) > 0 {
+		outStr.WriteString(writeSym)
+		writeSym = ""
+	}
+
+	return outStr.String(), nil
 }
