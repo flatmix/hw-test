931f6e30e4ab9457a1be802ab3dd3413aebc786e
Владислав Тащеев <v.tashcheev@automacon.ru>
2023-07-18 09:12:07 +0300

    HW3 is completed

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,59 @@
-package hw03frequencyanalysis
+package main
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"sort"
+	"strings"
+)
+
+type WordItem struct {
+	Count int
+	Word  string
+}
+
+func Top10(text string) []string {
+	words := strings.Fields(text)
+	wordsMap := make(map[string]int, 0)
+
+	for _, wordKey := range words {
+		word := strings.ToLower(strings.Trim(wordKey, `,-!.'"`))
+		if len(word) == 0 {
+			continue
+		}
+		entry, ok := wordsMap[word]
+		if !ok {
+			wordsMap[word] = 1
+		} else {
+			wordsMap[word] = entry + 1
+		}
+	}
+
+	wordsArr := make([]WordItem, len(wordsMap))
+
+	i := 0
+	for word, count := range wordsMap {
+		wordsArr[i] = WordItem{
+			Count: count,
+			Word:  word,
+		}
+		i++
+	}
+
+	sort.Slice(wordsArr, func(i, j int) bool {
+		if wordsArr[i].Count != wordsArr[j].Count {
+			return wordsArr[i].Count > wordsArr[j].Count
+		}
+		return wordsArr[i].Word < wordsArr[j].Word
+	})
+
+	result := []string{}
+
+	for iw, structWord := range wordsArr {
+		if iw < 10 {
+			result = append(result, structWord.Word)
+		} else {
+			break
+		}
+	}
+
+	return result
 }
